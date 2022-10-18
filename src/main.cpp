6346de12f4de576a47ad32178a6246ea016386e6
Matheus Castello <matheus@castello.eng.br>
2022-10-18 16:54:31 -0300

    src: main: Add timer
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main() {
+    std::cout << "Wait ..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(60000));
     std::cout << "Hello Torizon!" << std::endl;
 
     return 0;
